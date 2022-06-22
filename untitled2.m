064fc872148bbfcbb34d0fb2e150003767429ea7
Xiaodong Lei <x.lei@tum.de>
2022-06-23 00:51:53 +0200

    Finish Point Select

---
+++ untitled2.m
@@ -0,0 +1,17 @@
+xp = imread('oil-painting.png');
+
+fig = uifigure;
+ax = uiaxes(fig);
+imshow(xp, 'Parent', ax)
+% fig.HandleVisibility = 'on';
+% result = ginput(4);
+% [x,y] = getpts(ax);
+% imshow(xp);
+% h = drawpoint
+% x=round(x);
+% y=round(y);
+ax.YLim = [0 size(xp,1)];
+ax.XLim = [0 size(xp,2)];
+h = drawpoint(ax);
+% [x,y]
+% fig.HandleVisibility = 'off';
