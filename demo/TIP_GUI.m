c0fe911f233ff3de489c806df477651dc188ea26
fu <fu@fortiss.org>
2022-06-23 19:04:21 +0200

    no finish version to draw the five rectangle

--- demo/TIP_GUI.m
+++ demo/TIP_GUI.m
@@ -50,20 +50,16 @@ while 1,
   orx = round(orx);
   ory = round(ory);
  
-  % draw everything
-  imshow(im);
-  hold on;
-  irx = round([rx(1) rx(2) rx(2) rx(1) rx(1)]);
-  iry =  round([ry(1) ry(1) ry(2) ry(2) ry(1)]);
-  plot(irx,iry,'b'); 
-  plot([vx irx(1)], [vy iry(1)], 'r-.');
-  plot([orx(1) irx(1)], [ory(1) iry(1)], 'r');
-  plot([vx irx(2)], [vy iry(2)], 'r-.');
-  plot([orx(2) irx(2)], [ory(2) iry(2)], 'r');
-  plot([vx irx(3)], [vy iry(3)], 'r-.');
-  plot([orx(3) irx(3)], [ory(3) iry(3)], 'r');
-  plot([vx irx(4)], [vy iry(4)], 'r-.');
-  plot([orx(4) irx(4)], [ory(4) iry(4)], 'r');
-  hold off;
+  % find where the line from VP thru inner rectangle hits the edge of image
+  [ox,oy] = find_corner(vx,vy,irx(1),iry(1),0,0);
+  orx(1) = ox;  ory(1) = oy;
+  [ox,oy] = find_corner(vx,vy,irx(2),iry(2),xmax,0);
+  orx(2) = ox;  ory(2) = oy;
+  [ox,oy] = find_corner(vx,vy,irx(3),iry(3),xmax,ymax);
+  orx(3) = ox;  ory(3) = oy;
+  [ox,oy] = find_corner(vx,vy,irx(4),iry(4),0,ymax);
+  orx(4) = ox;  ory(4) = oy;
+  orx = round(orx);
+  ory = round(ory);
   drawnow;
 end;
