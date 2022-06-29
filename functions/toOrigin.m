26eea0f511f484c8fb0954ce0f5dda759f1d0789
Yuntian Yan <yuntian.yan@tum.de>
2022-06-29 21:13:53 +0200

    Replace toOrigin.m

--- functions/toOrigin.m
+++ functions/toOrigin.m
@@ -16,3 +16,4 @@ function [eye_3d, vp_3d, p1_3d, p2_3d, p3_3d, p4_3d, p5_3d, p6_3d, p7_3d, p8_3d,
     p10_3d = p10_3d - eye_3d;
     p11_3d = p11_3d - eye_3d;
     p12_3d = p12_3d - eye_3d;
+    eye_3d = [0;0;0];
