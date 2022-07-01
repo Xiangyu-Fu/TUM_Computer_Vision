b2b446e5f2a8e05c42589e62f80576781858650a
Yuntian Yan <yuntian.yan@tum.de>
2022-07-01 16:01:19 +0200

    Replace transPointCoordinates.m，输出均为行向量

--- functions/transPointCoordinates.m
+++ functions/transPointCoordinates.m
@@ -7,6 +7,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     p9_3d, p10_3d, p11_3d, p12_3d)
 
     transVP_3 = K_trans * [vp_3d';1];
+    transVP_3 = transVP_3';
     if transVP_3(3) ~= 0
         transVP_2(1) = transVP_3(1)/transVP_3(3);
         transVP_2(2) = transVP_3(2)/transVP_3(3);
@@ -16,6 +17,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP1_3 = K_trans * [p1_3d';1];
+    transP1_3 = transP1_3';
     if transP1_3(3) ~= 0
         transP1_2(1) = transP1_3(1)/transP1_3(3);
         transP1_2(2) = transP1_3(2)/transP1_3(3);
@@ -25,6 +27,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP2_3 = K_trans * [p2_3d';1];
+    transP2_3 = transP2_3';
     if transP2_3(3) ~= 0
         transP2_2(1) = transP2_3(1)/transP2_3(3);
         transP2_2(2) = transP2_3(2)/transP2_3(3);
@@ -34,6 +37,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP3_3 = K_trans * [p3_3d';1];
+    transP3_3 = transP3_3';
     if transP3_3(3) ~= 0
         transP3_2(1) = transP3_3(1)/transP3_3(3);
         transP3_2(2) = transP3_3(2)/transP3_3(3);
@@ -43,6 +47,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP4_3 = K_trans * [p4_3d';1];
+    transP4_3 = transP4_3';
     if transP4_3(3) ~= 0
         transP4_2(1) = transP4_3(1)/transP4_3(3);
         transP4_2(2) = transP4_3(2)/transP4_3(3);
@@ -52,6 +57,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP5_3 = K_trans * [p5_3d';1];
+    transP5_3 = transP5_3';
     if transP5_3(3) ~= 0
         transP5_2(1) = transP5_3(1)/transP5_3(3);
         transP5_2(2) = transP5_3(2)/transP5_3(3);
@@ -61,6 +67,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP6_3 = K_trans * [p6_3d';1];
+    transP6_3 = transP6_3';
     if transP6_3(3) ~= 0
         transP6_2(1) = transP6_3(1)/transP6_3(3);
         transP6_2(2) = transP6_3(2)/transP6_3(3);
@@ -70,6 +77,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP7_3 = K_trans * [p7_3d';1];
+    transP7_3 = transP7_3';
     if transP7_3(3) ~= 0
         transP7_2(1) = transP7_3(1)/transP7_3(3);
         transP7_2(2) = transP7_3(2)/transP7_3(3);
@@ -79,6 +87,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP8_3 = K_trans * [p8_3d';1];
+    transP8_3 = transP8_3';
     if transP8_3(3) ~= 0
         transP8_2(1) = transP8_3(1)/transP8_3(3);
         transP8_2(2) = transP8_3(2)/transP8_3(3);
@@ -88,6 +97,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP9_3 = K_trans * [p9_3d';1];
+    transP9_3 = transP9_3';
     if transP9_3(3) ~= 0
         transP9_2(1) = transP9_3(1)/transP9_3(3);
         transP9_2(2) = transP9_3(2)/transP9_3(3);
@@ -97,6 +107,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP10_3 = K_trans * [p10_3d';1];
+    transP10_3 = transP10_3';
     if transP10_3(3) ~= 0
         transP10_2(1) = transP10_3(1)/transP10_3(3);
         transP10_2(2) = transP10_3(2)/transP10_3(3);
@@ -106,6 +117,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP11_3 = K_trans * [p11_3d';1];
+    transP11_3 = transP11_3';
     if transP11_3(3) ~= 0
         transP11_2(1) = transP11_3(1)/transP11_3(3);
         transP11_2(2) = transP11_3(2)/transP11_3(3);
@@ -115,6 +127,7 @@ function [transVP_3, transP1_3, transP2_3, transP3_3, transP4_3, transP5_3,...
     end
     
     transP12_3 = K_trans * [p12_3d';1];
+    transP12_3 = transP12_3';
     if transP12_3(3) ~= 0
         transP12_2(1) = transP12_3(1)/transP12_3(3);
         transP12_2(2) = transP12_3(2)/transP12_3(3);
