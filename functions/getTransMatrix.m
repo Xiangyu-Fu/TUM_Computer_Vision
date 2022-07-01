ec46ad1a65fbabb189df4cc30ac9b10393b07a03
Yuntian Yan <yuntian.yan@tum.de>
2022-07-01 13:03:27 +0200

    Replace getTransMatrix.m，修复了z的问题

--- functions/getTransMatrix.m
+++ functions/getTransMatrix.m
@@ -2,6 +2,6 @@ function [K_trans, RT] = getTransMatrix(theta, phi, z, K)
     R = [cos(theta)              0         sin(theta);
          sin(theta) * sin(phi)   cos(phi)  -cos(theta) * sin(phi);
          -sin(theta) * cos(phi)  sin(phi)  cos(theta) * cos(phi)];
-    T = [0; 0; -z];
+    T = [0; 0; z];
     RT = [R T; 0 0 0 1];
     K_trans = K * RT;
