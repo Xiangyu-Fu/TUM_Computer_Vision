03b6a9cf22d6014de5c53feadbefffd93a08ace8
Xiaodong Lei <x.lei@tum.de>
2022-07-05 12:05:30 +0200

    Do a little change to GUI, delete some tables and 'go'-buttom in 'Tour' page.

--- functions/DrawFiveRects.m
+++ functions/DrawFiveRects.m
@@ -8,37 +8,37 @@ function [p11, p10, p4, p5, p9, p3, p12, p6,lmargin,rmargin,tmargin,bmargin,heig
 
     [height, width, channel] = size(bim);
 
-    app.RealWall(1,:) = [app.RealWall(1,1) + lmargin, app.RealWall(1,2) + tmargin];
-    app.RealWall(2,:) = [app.RealWall(2,1) + lmargin, app.RealWall(2,2) + tmargin];
-    app.RealWall(3,:) = [app.RealWall(3,1) + lmargin, app.RealWall(3,2) + tmargin];
-    app.RealWall(4,:) = [app.RealWall(4,1) + lmargin, app.RealWall(4,2) + tmargin];
+    app.RearWall(1,:) = [app.RearWall(1,1) + lmargin, app.RearWall(1,2) + tmargin];
+    app.RearWall(2,:) = [app.RearWall(2,1) + lmargin, app.RearWall(2,2) + tmargin];
+    app.RearWall(3,:) = [app.RearWall(3,1) + lmargin, app.RearWall(3,2) + tmargin];
+    app.RearWall(4,:) = [app.RearWall(4,1) + lmargin, app.RearWall(4,2) + tmargin];
     app.VanishingPoint = [app.VanishingPoint(1) + lmargin, app.VanishingPoint(2) + tmargin];
     app.expandImage = bim;
 
 
     p10(2) = tmargin;
-    p10(1) = round(find_line_x(vx,vy,app.RealWall(2,1),app.RealWall(2,2), p10(2) ));
+    p10(1) = round(find_line_x(vx,vy,app.RearWall(2,1),app.RearWall(2,2), p10(2) ));
 
     p12(1) = width - rmargin;
-    p12(2) = round(find_line_y(vx,vy,app.RealWall(2,1),app.RealWall(2,2), p12(1) ));
+    p12(2) = round(find_line_y(vx,vy,app.RearWall(2,1),app.RearWall(2,2), p12(1) ));
 
     p6(1) = width - rmargin;
-    p6(2) = round(find_line_y(vx,vy,app.RealWall(4,1),app.RealWall(4,2), p6(1) ));
+    p6(2) = round(find_line_y(vx,vy,app.RearWall(4,1),app.RearWall(4,2), p6(1) ));
 
     p4(2) = height - bmargin;
-    p4(1) = round(find_line_x(vx,vy,app.RealWall(4,1),app.RealWall(4,2), p4(2) ));
+    p4(1) = round(find_line_x(vx,vy,app.RearWall(4,1),app.RearWall(4,2), p4(2) ));
 
     p9(2) = tmargin;
-    p9(1) = round(find_line_x(vx,vy,app.RealWall(1,1),app.RealWall(1,2), p9(2) ));
+    p9(1) = round(find_line_x(vx,vy,app.RearWall(1,1),app.RearWall(1,2), p9(2) ));
 
     p3(2) = height - bmargin;
-    p3(1) = round(find_line_x(vx,vy,app.RealWall(3,1),app.RealWall(3,2), p3(2) ));
+    p3(1) = round(find_line_x(vx,vy,app.RearWall(3,1),app.RearWall(3,2), p3(2) ));
 
     p11(1) = lmargin;
-    p11(2) = round(find_line_y(vx,vy,app.RealWall(1,1),app.RealWall(1,2), p11(1) ));
+    p11(2) = round(find_line_y(vx,vy,app.RearWall(1,1),app.RearWall(1,2), p11(1) ));
 
     p5(1) = lmargin;
-    p5(2) = round(find_line_y(vx,vy,app.RealWall(3,1),app.RealWall(3,2), p5(1) ));
+    p5(2) = round(find_line_y(vx,vy,app.RearWall(3,1),app.RearWall(3,2), p5(1) ));
 
 
 
--- functions/GetFixedSplitedRectFunc.m
+++ functions/GetFixedSplitedRectFunc.m
@@ -9,10 +9,10 @@ function GetFixedSplitedRectFunc(app)
     % draw 5 rects and also 
     % return 4 points (right and left wall points)
     
-    p7 = app.RealWall(1,:);
-    p8 = app.RealWall(2,:);
-    p1 = app.RealWall(3,:);
-    p2 = app.RealWall(4,:);
+    p7 = app.RearWall(1,:);
+    p8 = app.RearWall(2,:);
+    p1 = app.RearWall(3,:);
+    p2 = app.RearWall(4,:);
     
     app.Vertex = [p1;p2;p3;p4;p5;p6;p7;p8;p9;p10;p11;p12];
 
@@ -23,21 +23,21 @@ function GetFixedSplitedRectFunc(app)
     app.UITable_2.Data = array2table(app.Vertex);
     
     
-    app.UITable_3.RowName={'vp'; 'eye'; '7'; '8'; '1'; '2'; '11'; '10'; '4'; '5';'9';'3';'12';'6'};
-    
-    vp = app.VanishingPoint;
-    
-    [vp_3d, p7_3d, p8_3d, p1_3d, p2_3d, p11_3d,...
-        p10_3d, p4_3d, p5_3d, p9_3d, p3_3d, p12_3d, p6_3d, eyep] = get3D(...
-        vp, p7, p8, p1, p2, p11, p10, p4, p5, p9, p3, p12, p6,height, width);
-    
-    points_3d = [vp_3d; eyep; p7_3d; p8_3d; p1_3d; p2_3d; p11_3d;...
-        p10_3d; p4_3d; p5_3d; p9_3d; p3_3d; p12_3d; p6_3d];
-    
-    app.UITable_3.Data = array2table(points_3d);
+%  
+%     
+%     vp = app.VanishingPoint;
+%     
+%     [vp_3d, p7_3d, p8_3d, p1_3d, p2_3d, p11_3d,...
+%         p10_3d, p4_3d, p5_3d, p9_3d, p3_3d, p12_3d, p6_3d, eyep] = get3D(...
+%         vp, p7, p8, p1, p2, p11, p10, p4, p5, p9, p3, p12, p6,height, width);
+%     
+%     points_3d = [vp_3d; eyep; p7_3d; p8_3d; p1_3d; p2_3d; p11_3d;...
+%         p10_3d; p4_3d; p5_3d; p9_3d; p3_3d; p12_3d; p6_3d];
+%     
+%     app.UITable_3.Data = array2table(points_3d);
     
 %     % the front wall
-    app.RealWall = splitTrapezoid (app.expandImage, p7, p8, p2, p1);  
+    app.RearWall = splitTrapezoid (app.expandImage, p7, p8, p2, p1);  
 %     
 %     
     app.Ceil = splitTrapezoid (app.expandImage, p9, p10, p8, p7);
