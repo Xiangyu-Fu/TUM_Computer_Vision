9dd0074951e2352aeb87bc8510607a67a1adce79
Stan Fu <2016mydream@gmail.com>
2022-07-11 14:51:15 +0200

    add resize function as well as remove left and right wall function

--- funcs/getNewTrapezoid.m
+++ funcs/getNewTrapezoid.m
@@ -1,4 +1,4 @@
-function result = getNewTrapezoid(img, p1, p2, p3, p4, p1_new, p2_new, p3_new, p4_new)
+function result = getNewTrapezoid(img, p1, p2, p3, p4, p1_new, p2_new, p3_new, p4_new, if_hide)
     % Upper left: p1
     % Upper right: p2
     % Lower right: p3
@@ -7,26 +7,40 @@ function result = getNewTrapezoid(img, p1, p2, p3, p4, p1_new, p2_new, p3_new, p
     xmax = size(img,2);
     ymax = size(img,1);
 
-    if (p1_new(1)+2*xmax)<=(p3_new(1)+2*xmax)&&(p1_new(2)+2*ymax)<=(p3_new(2)+2*ymax)
-
-       % Create perspective transformation that warps the cutted rectangle (trapezoid)
-       % coordinates to the traget shape (with 4 vertices).
-    
-        movingPoints = [p1; p2; p3; p4];
-        fixedPoints = [p1_new; p2_new; p3_new; p4_new];
-        
-    
-        tform = fitgeotrans(movingPoints, fixedPoints, 'Projective');
-    
-        % Create a reference coordinate system where the extent is the size of the image
-    
-        RA = imref2d([ymax xmax], [1 xmax], [1 ymax]);
-    
-        % Warp the image
-    
-        [result,r] = imwarp(img, tform, 'OutputView', RA);
+    if if_hide
+        if (p1_new(1)+2*xmax)<=(p3_new(1)+2*xmax)&&(p1_new(2)+2*ymax)<=(p3_new(2)+2*ymax)
+            % Create perspective transformation that warps the cutted rectangle (trapezoid)
+            % coordinates to the traget shape (with 4 vertices).
+            
+            movingPoints = [p1; p2; p3; p4];
+            fixedPoints = [p1_new; p2_new; p3_new; p4_new];
+            
+            
+            tform = fitgeotrans(movingPoints, fixedPoints, 'Projective');
+            
+            % Create a reference coordinate system where the extent is the size of the image
+            
+            RA = imref2d([ymax xmax], [1 xmax], [1 ymax]);
+            
+            % Warp the image
+            
+            [result,~] = imwarp(img, tform, 'OutputView', RA);
+        else
+            result = zeros(size(img));
+        end
     else
-        result = zeros(size(img));
+            movingPoints = [p1; p2; p3; p4];
+            fixedPoints = [p1_new; p2_new; p3_new; p4_new];
+            
+        
+            tform = fitgeotrans(movingPoints, fixedPoints, 'Projective');
+        
+            % Create a reference coordinate system where the extent is the size of the image
+        
+            RA = imref2d([ymax xmax], [1 xmax], [1 ymax]);
+        
+            % Warp the image
+        
+            [result,~] = imwarp(img, tform, 'OutputView', RA);
     end
-
 end
