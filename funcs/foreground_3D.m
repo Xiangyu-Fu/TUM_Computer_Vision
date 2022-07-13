a3925b9e9230d26656744fc5a7f1f40302e1826f
Xiaodong Lei <x.lei@tum.de>
2022-07-14 00:02:56 +0200

    add a new feature. Now it supports selecting multiple foregrounds and imaging.

--- funcs/foreground_3D.m
+++ funcs/foreground_3D.m
@@ -10,24 +10,29 @@ function foreground_3D(app)
     FG_recoord(:,1) = (app.FG(:,1) - app.VP(1)) / img_x;
     FG_recoord(:,2) = -(app.FG(:,2) - app.VP(2)) / img_y;
 
-
-    ground = FG_recoord(3,2);
-
     eyep = [0, 0, 0];
 
-    grad = -(1.0+ eyep(2) ) / (ground - eyep(2) );
 
-    z = grad * (-1.0 - eyep(3) ) + eyep(3);
 
-    for i = 1:4
-        
-        height = ( FG_recoord(i,2) - ground ) / -1*z;
-        grad = (height - eyep(2) - 1) / ( FG_recoord(i,2) - eyep(2) );
+    for j = 1:app.FG_count
 
-        app.FG_3d(i,1) = grad * ( FG_recoord(i,1) - eyep(1) ) + eyep(1);
-        app.FG_3d(i,2) = height - 1;
-        app.FG_3d(i,3) = grad * (-1 - eyep(3) ) + eyep(3);
+        ground = FG_recoord(4*j-1,2);
+    
+        grad = -(1.0+ eyep(2) ) / (ground - eyep(2) );
+    
+        z = grad * (-1.0 - eyep(3) ) + eyep(3);
 
+        for i = 4*j-3:4*j
+            
+            height = ( FG_recoord(i,2) - ground ) / -1*z;
+            grad = (height - eyep(2) - 1) / ( FG_recoord(i,2) - eyep(2) );
+    
+            app.FG_3d(i,1) = grad * ( FG_recoord(i,1) - eyep(1) ) + eyep(1);
+            app.FG_3d(i,2) = height - 1;
+            app.FG_3d(i,3) = grad * (-1 - eyep(3) ) + eyep(3);
+            
+        end
     end
+
 
 end
--- funcs/getForegroundPoints2D.m
+++ funcs/getForegroundPoints2D.m
@@ -1,5 +1,5 @@
 
-function  getForegroundPoints2D(app)
+function  FG_points_2d = getForegroundPoints2D(app, FG_3d)
 % getForegroundPoints2D.m uses getFg3D.m and the parameter app.K_trans
 % /////////////////////////////////////////////////////////////
 % FG_p1,FG_p2,FG_p3,FG_p4 Pixel coordinates of four Foreground points
@@ -10,13 +10,14 @@ function  getForegroundPoints2D(app)
 
 %[FG_p1_3d,FG_p2_3d,FG_p3_3d,FG_p4_3d] = foreground_3D(app); % get Foreround points 3D coordinates
 
-    transFG_p1_3 = app.K_trans * [app.FG_3d(1,:)';1];
+
+    transFG_p1_3 = app.K_trans * [FG_3d(1,:)';1];
     transFG_p1_3 = transFG_p1_3';
-    transFG_p2_3 = app.K_trans * [app.FG_3d(2,:)';1];
+    transFG_p2_3 = app.K_trans * [FG_3d(2,:)';1];
     transFG_p2_3 = transFG_p2_3';
-    transFG_p3_3 = app.K_trans * [app.FG_3d(3,:)';1];
+    transFG_p3_3 = app.K_trans * [FG_3d(3,:)';1];
     transFG_p3_3 = transFG_p3_3';
-    transFG_p4_3 = app.K_trans * [app.FG_3d(4,:)';1];
+    transFG_p4_3 = app.K_trans * [FG_3d(4,:)';1];
     transFG_p4_3 = transFG_p4_3';
 
     if transFG_p1_3(3) >= 0
@@ -26,52 +27,39 @@ function  getForegroundPoints2D(app)
     end
 
 
-%      if showforeground 
-        if transFG_p1_3(3) ~= 0
-            transFG_p1_2(1) = transFG_p1_3(1)/transFG_p1_3(3);
-            transFG_p1_2(2) = transFG_p1_3(2)/transFG_p1_3(3);
-        else
-            transFG_p1_2(1) = transFG_p1_3(1)/0.01;
-            transFG_p1_2(2) = transFG_p1_3(2)/0.01;
-        end
-        
-        if transFG_p2_3(3) ~= 0
-            transFG_p2_2(1) = transFG_p2_3(1)/transFG_p2_3(3);
-            transFG_p2_2(2) = transFG_p2_3(2)/transFG_p2_3(3);
-        else
-            transFG_p2_2(1) = transFG_p2_3(1)/0.01;
-            transFG_p2_2(2) = transFG_p2_3(2)/0.01;
-        end
-        
-        if transFG_p3_3(3) ~= 0
-            transFG_p3_2(1) = transFG_p3_3(1)/transFG_p3_3(3);
-            transFG_p3_2(2) = transFG_p3_3(2)/transFG_p3_3(3);
-        else
-            transFG_p3_2(1) = transFG_p3_3(1)/0.01;
-            transFG_p3_2(2) = transFG_p3_3(2)/0.01;
-        end
-        
-        if transFG_p4_3(3) ~= 0
-            transFG_p4_2(1) = transFG_p4_3(1)/transFG_p4_3(3);
-            transFG_p4_2(2) = transFG_p4_3(2)/transFG_p4_3(3);
-        else
-            transFG_p4_2(1) = transFG_p4_3(1)/0.01;
-            transFG_p4_2(2) = transFG_p4_3(2)/0.01;
-        end
+    if transFG_p1_3(3) ~= 0
+        transFG_p1_2(1) = transFG_p1_3(1)/transFG_p1_3(3);
+        transFG_p1_2(2) = transFG_p1_3(2)/transFG_p1_3(3);
+    else
+        transFG_p1_2(1) = transFG_p1_3(1)/0.01;
+        transFG_p1_2(2) = transFG_p1_3(2)/0.01;
+    end
+    
+    if transFG_p2_3(3) ~= 0
+        transFG_p2_2(1) = transFG_p2_3(1)/transFG_p2_3(3);
+        transFG_p2_2(2) = transFG_p2_3(2)/transFG_p2_3(3);
+    else
+        transFG_p2_2(1) = transFG_p2_3(1)/0.01;
+        transFG_p2_2(2) = transFG_p2_3(2)/0.01;
+    end
+    
+    if transFG_p3_3(3) ~= 0
+        transFG_p3_2(1) = transFG_p3_3(1)/transFG_p3_3(3);
+        transFG_p3_2(2) = transFG_p3_3(2)/transFG_p3_3(3);
+    else
+        transFG_p3_2(1) = transFG_p3_3(1)/0.01;
+        transFG_p3_2(2) = transFG_p3_3(2)/0.01;
+    end
+    
+    if transFG_p4_3(3) ~= 0
+        transFG_p4_2(1) = transFG_p4_3(1)/transFG_p4_3(3);
+        transFG_p4_2(2) = transFG_p4_3(2)/transFG_p4_3(3);
+    else
+        transFG_p4_2(1) = transFG_p4_3(1)/0.01;
+        transFG_p4_2(2) = transFG_p4_3(2)/0.01;
+    end
 
-%      else
-%         transFG_p1_2(1)=0;
-%         transFG_p1_2(2)=0;
-%         transFG_p2_2(1)=0;
-%         transFG_p2_2(2)=0;
-%         transFG_p3_2(1)=0;
-%         transFG_p3_2(2)=0;
-%         transFG_p4_2(1)=0;
-%         transFG_p4_2(2)=0;
-%      end
-
-        FG_points_2d = [transFG_p1_2; transFG_p2_2; transFG_p3_2; transFG_p4_2];
-        app.FG_points_2d = round(FG_points_2d);
+        FG_points_2d = round([transFG_p1_2; transFG_p2_2; transFG_p3_2; transFG_p4_2]);
 
 
     
